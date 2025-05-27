f65a9a4883ebefe1e0ff199fa45122a3eda5bf3d
Maulik <maulik.shah@nagarro.com>
2025-05-27 12:13:34 +0530

    updated logo

--- src/App.js
+++ src/App.js
@@ -6,18 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <b>Azure Training</b>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://msngrazuretraining.blob.core.windows.net/azure-training-demo/nagarro-logo.gif" className="App-logo" alt="logo" />
       </header>
     </div>
   );
